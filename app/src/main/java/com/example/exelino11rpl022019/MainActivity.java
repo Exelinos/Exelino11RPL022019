0cfa511680bfb469350fb1cb027e3013a5091556
Exelinos <excel.10rpl2.2k19@gmail.com>
2020-08-15 20:40:48 +0700

    add SP and CardView Menu

--- app/src/main/java/com/example/exelino11rpl022019/MainActivity.java
+++ app/src/main/java/com/example/exelino11rpl022019/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.exelino11rpl022019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    EditText txtname, txtpass;
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login",MODE_PRIVATE);
+        txtname = findViewById(R.id.txtnama);
+        txtpass = findViewById(R.id.txtpass);
+        btnlogin = findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtname.getText().toString().equalsIgnoreCase("admin")&&
+                txtpass.getText().toString().equalsIgnoreCase("admin")) {
+                    editor = pref.edit();
+                    editor.putString("username", txtname.getText().toString() );
+                    editor.putString("status", "login");
+                    editor.apply();
+                    startActivity(new Intent(getApplicationContext(), MainMenu.class));
+                    finish();
+                }
+            }
+        });
     }
 }
